7a082387acf42d41724cf437bb81d91052853c5e
Zane Finucane <leila@Zanes-MacBook-Air.local>
2019-11-16 10:12:39 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running");
+var cardOne = "King";
+var CardTwo = "Queen";
+var CardThree = "King";
+var CardFour = "Queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + CardTwo)
